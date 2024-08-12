ac46685a590c98986712d0dcde1577091989d6ac
Evance Onyango <devlans305@gmail.com>
2024-08-12 19:09:38 +0300

    added the ZombieFcatory Class contract

---
+++ main.sol
@@ -0,0 +1,45 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+
+    // declare our event here
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    function _createZombie(string memory _name, uint _dna) private {
+        zombies.push(Zombie(_name, _dna));
+        // and fire it here
+    }
+
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+
+    //As simple way to listen to the events 
+
+    event addIntegers(uint _x, uint _y, result);
+
+    function add(_x, _y, result) public returns (uint){
+       uint result =  _x + _y;
+
+       emit addIntegers(_x, _y, result)
+
+       return result 
+        
+    }
+
+}
